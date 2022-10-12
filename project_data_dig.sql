1e2f3e534097ec42087947d5b61435d758de2c2f
vgroves19 <115603371+vgroves19@users.noreply.github.com>
2022-10-11 23:09:27 -0400

    Update project_data_dig.sql

--- project_data_dig.sql
+++ project_data_dig.sql
@@ -1,3 +1,12 @@
+/*We’ve curated a set of interesting data sets for you: NASA astronauts, Superbowl results, Pokemon stats, NBA players, Top movies, Top countries by population, Solar system objects by size, Marvel characters, Furniture store sales, Earned KA badges, Winston's donut logs, Card game results, and NFL draft picks.
+
+Pick one of those data sets or create a data set like that, and use advanced SELECT queries to discover things about the data. What sort of questions might one have about that data, like if they were using it for an app or a business idea? Here are some ideas:
+
+What are average, max, and min values in the data?
+What about those numbers per category in the data (using HAVING)?
+What ways are there to group the data values that don’t exist yet (using CASE)?
+What interesting ways are there to filter the data (using AND/OR)?*/
+
 /* My solutions are below the insert values */
 
 /* Put your data in here and query it! */
