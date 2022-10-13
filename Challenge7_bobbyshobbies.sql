06417728c45841e0fa50ee5f3f4d88d0439aded7
vgroves19 <115603371+vgroves19@users.noreply.github.com>
2022-10-13 15:13:21 -0400

    Create Challenge7_bobbyshobbies.sql

---
+++ Challenge7_bobbyshobbies.sql
@@ -0,0 +1,41 @@
+/* Challenge: Bobby's Hobbies
+
+CREATE TABLE persons (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    age INTEGER);
+    
+INSERT INTO persons (name, age) VALUES ("Bobby McBobbyFace", 12);
+INSERT INTO persons (name, age) VALUES ("Lucy BoBucie", 25);
+INSERT INTO persons (name, age) VALUES ("Banana FoFanna", 14);
+INSERT INTO persons (name, age) VALUES ("Shish Kabob", 20);
+INSERT INTO persons (name, age) VALUES ("Fluffy Sparkles", 8);
+
+CREATE table hobbies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    person_id INTEGER,
+    name TEXT);
+    
+INSERT INTO hobbies (person_id, name) VALUES (1, "drawing");
+INSERT INTO hobbies (person_id, name) VALUES (1, "coding");
+INSERT INTO hobbies (person_id, name) VALUES (2, "dancing");
+INSERT INTO hobbies (person_id, name) VALUES (2, "coding");
+INSERT INTO hobbies (person_id, name) VALUES (3, "skating");
+INSERT INTO hobbies (person_id, name) VALUES (3, "rowing");
+INSERT INTO hobbies (person_id, name) VALUES (3, "drawing");
+INSERT INTO hobbies (person_id, name) VALUES (4, "coding");
+INSERT INTO hobbies (person_id, name) VALUES (4, "dilly-dallying");
+INSERT INTO hobbies (person_id, name) VALUES (4, "meowing");
+
+/* Select the 2 tables with a join so that you can see each person's name next to their hobby. */
+SELECT p.name, h.name
+FROM persons as p
+JOIN hobbies as h
+ON p.id = h.person_id;
+
+/* add an additional query that shows only the name and hobbies of 'Bobby McBobbyFace', using JOIN combined with WHERE. */
+SELECT p.name, h.name
+FROM persons as p 
+JOIN hobbies as h
+ON p.id = h.person_id
+WHERE p.name LIKE "Bobby McBobbyFace"
